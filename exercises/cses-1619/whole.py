39e479a3178e6679f2362c74347a807fd50f4b80
Cheng Sheng <jeru.sheng@gmail.com>
2026-04-28 21:35:22 +0200

    separate library for the tests datasource, and cses-1619.

---
+++ exercises/cses-1619/whole.py
@@ -0,0 +1,75 @@
+# Copyright 2026 Cheng Sheng
+# SPDX-License-Identifier: Apache-2.0
+#
+# https://cses.fi/problemset/task/1619
+#
+# Move the DataSource to a separate library.
+
+import os
+os.environ["JAX_PLATFORMS"] = "cpu"
+os.environ["JAX_ENABLE_X64"] = "1"
+
+import collections
+from functools import partial
+import os.path
+from typing import Any
+
+import grain
+import jax
+from jax import numpy as jnp
+from jax import lax
+from jaxtyping import Array, Int64
+import numpy
+
+from my_lib.tests_data_source import (
+    TestsDataSource,
+    INPUT_FIELDNAME,
+    ANSWER_FIELDNAME,
+)
+
+
+@jax.jit
+def solve_in_jax(a: Int64[Array, 'N']) -> Int64[Array, '1']:
+    n = a.shape[0]
+    signed_times = jnp.sort(a * 2 + jnp.arange(n) % 2)
+    signs = jnp.where(signed_times < 2, 0,
+                      jnp.where(signed_times % 2 == 0, +1, -1)
+                      ).astype(jnp.int64)
+    return jnp.max(jnp.add.accumulate(signs))
+
+
+def solve(content: str) -> str:
+    lines = content.strip().split()
+    arr = jnp.array(numpy.fromiter(map(int, lines[1:]), dtype=jnp.int64))
+    assert arr.shape[0] % 2 == 0
+
+    n = 1
+    while n < arr.shape[0]: n *= 2
+    # Limit the variety of array lengths to not explode the number of copies
+    # that jax.jit produces.
+    arr = jnp.pad(arr, (0, n - arr.shape[0]), constant_values=0)
+
+    s = solve_in_jax(arr)
+    return str(int(s))
+
+
+def solve_and_check(record: dict):
+    out = solve(record[INPUT_FIELDNAME])
+    ans = record[ANSWER_FIELDNAME]
+    assert out == ans.strip(), f'{out} vs {ans}'
+
+
+def main():
+    import sys
+    ds = TestsDataSource(sys.argv[1])
+    dl = grain.load(ds, num_epochs=2)
+    num = 0
+    for record in dl:
+        print(f'Testing {num}...')
+        solve_and_check(record)
+        num += 1
+    print(f'All {num} tests passed.')
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/my_lib/tests_data_source.py
@@ -0,0 +1,46 @@
+# Copyright 2026 Cheng Sheng
+# SPDX-License-Identifier: Apache-2.0
+
+import glob
+import os.path
+
+import grain.sources
+
+
+INPUT_FIELDNAME = "input"
+ANSWER_FIELDNAME = "answer"
+
+
+class TestsDataSource(grain.sources.RandomAccessDataSource[dict]):
+    """A data source backed by a directory of tests.
+
+    The tests should have file patterns X.in and X.out, X being a number.
+    """
+
+    def __init__(self, directory, in_ext='.in', ans_ext='.out'):
+        self._file_bases = _get_tests(directory, in_ext, ans_ext)
+        self._in_ext = in_ext
+        self._ans_ext = ans_ext
+
+    def __len__(self) -> int:
+        return len(self._file_bases)
+
+    def __getitem__(self, index: int) -> tuple[str, str]:
+        fn = self._file_bases[index]
+        with open(fn + self._in_ext) as f:
+            content1 = f.read()
+        with open(fn + self._ans_ext) as f:
+            content2 = f.read()
+        return {INPUT_FIELDNAME: content1, ANSWER_FIELDNAME: content2}
+
+
+def _get_tests_basenames(directory, ext):
+    files = glob.glob(os.path.join(directory, '*' + ext))
+    return sorted(os.path.splitext(os.path.basename(f))[0] for f in files)
+
+
+def _get_tests(directory, in_ext, ans_ext):
+    in_files = _get_tests_basenames(directory, in_ext)
+    ans_files = _get_tests_basenames(directory, ans_ext)
+    assert in_files == ans_files
+    return [os.path.join(directory, f) for f in in_files]
